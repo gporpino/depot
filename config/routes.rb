5fc69e31094ce63a3b2c706b100f359a3363a0bc
Gustavo Porpino <gporpino@icloud.com>
2013-07-07 13:37:22 -0300

    Apply migrate of quantity line items into cart

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Depot::Application.routes.draw do
+  resources :line_items
+
+
+  resources :carts
+
+
+  get "store/index"
+
   resources :products
+
+
+  # You can have the root of your site routed with "root" # just remember to delete public/index.html.
+  # root :to => "welcome#index"
+  root :to => 'store#index', :as => 'store'
 
 
   # The priority is based upon order of creation:
